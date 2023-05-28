df8fe5118a9f2e669f0aa90c32b540e1bae8ffed
Fergus Miller Kerins <fergus.mk93@gmail.com>
2023-05-28 17:50:05 +0200

    setup db

--- app/models/models.py
+++ app/models/models.py
@@ -1,17 +1,62 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 db = SQLAlchemy()
-
+ma = Marshmallow()
 
 class Todo(db.Model):
-    __tablename__ = "todos"
+    __tablename__ = "todo"
+    id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String, db.ForeignKey("user.email"))
+    content = db.Column(db.String, nullable=False)
+    priority = db.Column(db.Integer, nullable=False, default=1)
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
-    description = db.Column(db.String(), nullable=False)
-    due_date = db.Column(db.DateTime, nullable=True)
+
+class TodoSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Todo
+        load_instance = True
+        sqla_Session = db.session
+        include_fk = True  # Also need this so marshmallow recognises person_id during serialization
+    user_email = fields.Str()
+    content = fields.Str()
+    priority = fields.Integer()
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+
+    todos = db.relationship(
+        Todo,
+        backref="user",
+        cascade="all, delete, delete-orphan",
+        single_parent=True
+    )
 
     def __repr__(self):
-        return f"<Todo {self.id}, {self.completed}, {self.description}>"
- 
+        return f"User {self.first_name} {self.last_name} with email {self.email}"
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+        sqla_Session = db.session
+        include_relationships = True  # This means it will also go into neighbouring schema
+
+    email = fields.Str()
+    first_name = fields.Str()
+    last_name = fields.Str()
+    todos = fields.Nested(TodoSchema, many=True)
+
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
--- app/populate_db.py
+++ app/populate_db.py
@@ -1,9 +1,44 @@
-from app import app
-from models import Todo, db
+from app import create_app, db
+from models.models import User
+
+app = create_app()
 
 with app.app_context():
-    for i in range(1, 6):
-        todo = Todo(description=f"Sample Task {i}")
-        db.session.add(todo)
+    # Create five example users
+    users_data = [
+        {
+            'email': 'John@example.com',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        },
+        {
+            'email': 'Jane@example.com',
+            'first_name': 'Jane',
+            'last_name': 'Smith'
+        },
+        {
+            'email': 'Alice@example.com',
+            'first_name': 'Alice',
+            'last_name': 'Johnson'
+        },
+        {
+            'email': 'Bob@example.com',
+            'first_name': 'Bob',
+            'last_name': 'Williams'
+        },
+        {
+            'email': 'Emily@example.com',
+            'first_name': 'Emily',
+            'last_name': 'Brown'
+        }
+    ]
+
+    # Add the users to the database
+    for user_data in users_data:
+        user = User(**user_data)
+        db.session.add(user)
+
+    # Commit the changes
     db.session.commit()
-    print("Successfully populated the Todo table with 5 sample entries!")
+
+print("Users have been added to the database.")
