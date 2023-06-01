575968405c866d532a628bd823e2876862bc3e5e
Fergus Miller Kerins <fergus.mk93@gmail.com>
2023-06-01 19:31:17 +0200

    tidied comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,22 @@
 import os
 from flask import Flask
 from flask_migrate import Migrate
-# from dotenv import load_dotenv # REMOVED
 
 from app.routes.routes import init_routes
 from app.extensions.extensions import db, ma
 from app.models.models import User, Todo
-from app.config.config import Config # here
+from app.config.config import Config
 
 def create_app():
     app = Flask(__name__)
-    # load_dotenv()  # take environment variables from .env. # HERE
+    app.config.from_object(Config) 
 
-    # secret_key = os.getenv("SECRET_KEY")# HERE
-    # if secret_key is None: # HERE
-    #     raise ValueError("No SECRET_KEY set for Flask application") # HERE
+    db.init_app(app) 
+    ma.init_app(app) 
+    migrate = Migrate(app, db) 
 
-    # db_uri = os.getenv("SQLALCHEMY_DATABASE_URI") # HERE
-    # if db_uri is None: # HERE
-    #     raise ValueError("No SQLALCHEMY_DATABASE_URI set for Flask application") # HERE
-
-    # app.config["SECRET_KEY"] = secret_key # REMOVED
-    # app.config["SQLALCHEMY_DATABASE_URI"] = db_uri  # REMOVED
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # reduce overhead  # REMOVED
-    app.config.from_object(Config)  # new line here
-
-    db.init_app(app) # Here
-    ma.init_app(app) # Here
-    migrate = Migrate(app, db) # new line here
-
-    with app.app_context():  # new line here
-        db.create_all()  # new line here
+    with app.app_context():     
+        db.create_all()
 
     init_routes(app)
 
