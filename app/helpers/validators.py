ccdabc7679ea8d23f6b18efed9a2e8bec857b7bd
Fergus Miller Kerins <fergus.mk93@gmail.com>
2023-06-05 21:40:37 +0200

    added password validaion

--- app/helpers/validators.py
+++ app/helpers/validators.py
@@ -1,5 +1,6 @@
+import re
 from email_validator import validate_email, EmailNotValidError
-from app.responses.fail_response import email_not_valid, name_not_alpha, wrong_len_response
+from app.responses.fail_response import email_not_valid, name_not_alpha, wrong_len_response, password_not_valid
 
 def check_email_is_valid(email: str):
     """Checks if email is valid with characters both sides of an '@' symbol"""
@@ -17,5 +18,19 @@ def check_name_is_valid(name: str):
         return wrong_len_response(choice="short", type_describe="Name")
     elif len(name)>50:
         return wrong_len_response(choice="long", type_describe="Name")
+    else:
+        pass
+
+
+def check_password_is_valid(password: str):
+    """Checks if password is at least 5 characters, contains a digit and a special character."""
+    if len(password) < 5:
+        return password_not_valid()
+    
+    elif not re.search(r'\d', password):
+        return password_not_valid()
+    
+    elif not re.search(r'\W', password):
+        return password_not_valid()
     else:
         pass
