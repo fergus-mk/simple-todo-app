c2b9d6f17299b7d64ecd8f7eb827d18e3491b504
Fergus Miller Kerins <fergus.mk93@gmail.com>
2023-06-10 13:59:37 +0200

    added todo routes

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,48 +1,65 @@
-from flask import jsonify, request # ADDED request
+from flask import jsonify, request
 
-from app.models.test_ma_model import user_schema
-from app.responses.fail_response import user_not_provided
-from app.crud import user_crud
+from app.crud import user_crud, todo_crud
 from app.auth.auth import login, token_required
 
-def init_routes(app):
 
-    
-    @app.route("/api/test_marshmallow", methods=["GET"])
-    def test_marshmallow():
-        # Test marshmallow by serializing some data
-        user = {"name": "Fergus Dergus", "email": "john@example.com"}
-        return user_schema.dump(user), 200
-    
-    # @app.route('/user/<email>')
-    # def read_one_user(email: str):
-    #     """Reads one user, selected by email"""
-    #     return user_crud.get_user(email)
-    
-    # @token_required
-    # @app.route('/api/user/get_user')
-    # def get_user_route():
-    #     return user_crud.get_user()
+def init_user_routes(app):
 
-    @token_required
-    @app.route('/api/user/get_user')
-    def get_user_route():
-        """Reads one user, selected by email"""
-        return user_crud.get_user()
-
-    
-    @app.route('/user', methods=['POST']) # ADDED - UNSURE ABOUT THIS
+    @app.route('/api/user', methods=['POST'])
     def post_user():
         user = request.get_json()
-        if not user:
-            return user_not_provided   # THIS ISN't working - already error from no JSON
-        try:
-            new_user, status = user_crud.create_user(user)
-            return jsonify(new_user), status
-        except Exception as e:
-            return jsonify({"message": str(e)}), 400
+        new_user, status = user_crud.create_user(user)
         
-    # Auth
-    @app.route('/login', methods=['POST'])
+        return jsonify(new_user), status
+
+    @token_required
+    @app.route('/api/user', methods=['GET'])
+    def get_user():
+        """Reads current user"""
+        return user_crud.get_user()
+    
+    @token_required
+    @app.route('/api/user', methods=['PATCH'])
+    def update_user():
+        return user_crud.update_user()
+
+    @token_required
+    @app.route('/api/user', methods=['DELETE'])
+    def delete_user():
+        return user_crud.delete_user()
+
+def init_auth_routes(app):
+
+    @app.route('/api/login', methods=['POST'])
     def login_route():
         return login() 
+
+def init_todo_routes(app):
+
+    @token_required
+    @app.route('/api/todo', methods=['POST'])
+    def create_todo():
+        todo = request.get_json()
+        return todo_crud.create_todo_for_user(todo) 
+    
+    @token_required
+    @app.route('/api/todo/<int:todo_id>', methods=['GET'])
+    def read_todo(todo_id):
+        return todo_crud.read_one_todo(todo_id)
+
+    @token_required
+    @app.route('/api/todos', defaults={'priority': None}, methods=['GET'])
+    @app.route('/api/todos/<int:priority>', methods=['GET'])
+    def read_todos(priority):
+        return todo_crud.read_user_todos(priority)
+    
+    @token_required
+    @app.route('/api/todo/<int:todo_id>', methods=['PATCH'])
+    def update_todo(todo_id):
+        return todo_crud.update_todo(todo_id)
+
+    @token_required
+    @app.route('/api/todo/<int:todo_id>', methods=['DELETE'])
+    def delete_todo(todo_id):
+        return todo_crud.delete_todo(todo_id)
