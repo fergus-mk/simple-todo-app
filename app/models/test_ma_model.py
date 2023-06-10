d3cd2580143730f7ad0acb39c8cd298a0066785f
Fergus Miller Kerins <fergus.mk93@gmail.com>
2023-06-10 14:02:59 +0200

    removed

--- app/models/test_ma_model.py
+++
@@ -1,9 +0,0 @@
-from app.extensions.extensions import ma
-
-# Create a simple Marshmallow schema
-class UserSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ("name", "email")
-
-user_schema = UserSchema()
